beb86fe892d7d16a91749347a74ea7c5e2a286a5
ayoubzulfiqar <ayoubzulfiqar3@gmail.com>
2023-12-25 14:33:54 -0800

    aoc

--- Dart/Day25/day_of_christmas.dart
+++ Dart/Day25/day_of_christmas.dart
@@ -1,148 +1,69 @@
+import 'dart:collection';
 import 'dart:io';
 
-class Node {
-  late String name;
-  late Map<Edge, Node> edges;
-  bool traveled = false;
-
-  Node(this.name) : edges = {};
-}
-
-class Edge {
-  bool traveled = false;
-}
-
-class QueueItem {
-  late Edge? edge;
-  late Node node;
-  late QueueItem? previous;
-
-  QueueItem({required this.node, this.previous, required this.edge});
-}
-
-class Graph {
-  late Map<String, Node> nodes;
-  late List<Edge> edges;
-
-  Graph()
-      : nodes = {},
-        edges = [];
-
-  void resetNodes() {
-    for (var node in nodes.values) {
-      node.traveled = false;
-    }
-  }
-
-  void resetEdges() {
-    for (var edge in edges) {
-      edge.traveled = false;
-    }
-  }
-
-  bool removeShortestPath(Node source, Node dest) {
-    var queue = <QueueItem>[QueueItem(node: source, edge: null)];
-    var found = false;
-
-    while (queue.isNotEmpty) {
-      var current = queue.removeAt(0);
-
-      if (current.node == dest) {
-        for (var itr = current; itr.edge != null; itr = itr.previous!) {
-          itr.edge?.traveled = true;
-        }
-        found = true;
-        break;
-      }
-
-      for (var entry in current.node.edges.entries) {
-        var e = entry.key;
-        var n = entry.value;
-        if (e.traveled || n.traveled) {
-          continue;
-        }
-        n.traveled = true;
-        queue.add(QueueItem(edge: e, node: n, previous: current));
-      }
-    }
-
-    resetNodes();
-    return found;
-  }
-
-  bool cutPaths(Node source, Node dest, int pathNum) {
-    var complete = true;
-    for (var i = 0; i < pathNum; i++) {
-      if (!removeShortestPath(source, dest)) {
-        complete = false;
-        break;
-      }
-    }
-    resetEdges();
-    return complete;
-  }
-
-  List<Node> split(int cuts) {
-    var g1 = <Node>[];
-    var g2 = <Node>[];
-
-    Node? source;
-    for (var n in nodes.values) {
-      source = n;
-      break;
-    }
-    g1.add(source!);
-
-    for (var dest in nodes.values) {
-      if (source == dest) {
-        continue;
-      }
-
-      if (cutPaths(source, dest, cuts + 1)) {
-        g1.add(dest);
-      } else {
-        g2.add(dest);
-      }
-    }
-    return [...g1, ...g2];
-  }
-}
-
-Graph multipleOfTwoGroups(List<String> input) {
-  var nodes = <String, Node>{};
-  for (var line in input) {
-    var parts = line.split(": ");
-    var name = parts[0];
-    nodes[name] = Node(name);
-  }
-
-  var edges = <Edge>[];
-  for (var line in input) {
-    var parts = line.split(": ");
-    var sourceName = parts[0];
-    var destNames = parts[1].split(" ");
-    var source = nodes[sourceName]!;
-    for (var destName in destNames) {
-      var dest = nodes[destName] ?? Node(destName);
-      var newEdge = Edge();
-      edges.add(newEdge);
-      source.edges[newEdge] = dest;
-      dest.edges[newEdge] = source;
-    }
-  }
-
-  return Graph()
-    ..nodes = nodes
-    ..edges = edges;
-}
-
 void main() {
-  var input = <String>[];
-  var file = File("Dart/Day25/input.txt");
-  var lines = file.readAsLinesSync();
-  input.addAll(lines);
+  var input = File('Dart/Day25/input.txt').readAsLinesSync();
 
-  var groups = multipleOfTwoGroups(input);
-  var result = groups.split(3);
-  print(result.length * result.length);
+  var dotFile = StringBuffer();
+  dotFile.writeln("graph {");
+
+  var wires = <String, Set<String>>{};
+  for (var line in input) {
+    var parts = line.split(": ");
+    var wire = parts[0].trim();
+    var connections = parts[1].trim().split(" ").toList();
+
+    wires[wire] ??= {}; // Ensure wires[wire] is initialized as an empty set.
+
+    for (var connection in connections) {
+      // Generate content for a GraphViz graph.
+      dotFile.writeln("    $wire -- $connection");
+
+      wires[connection] ??= {};
+
+      wires[wire]?.add(connection);
+      wires[connection]?.add(wire);
+    }
+  }
+
+  dotFile.writeln("}");
+  File('graph.dot').writeAsStringSync(dotFile.toString());
+
+  // Edges to be removed
+  const a1 = "cbl";
+  const b1 = "vmq";
+
+  const a2 = "bvz";
+  const b2 = "nvf";
+
+  const a3 = "klk";
+  const b3 = "xgz";
+
+  wires[a1]?.remove(b1);
+  wires[b1]?.remove(a1);
+  wires[a2]?.remove(b2);
+  wires[b2]?.remove(a2);
+  wires[a3]?.remove(b3);
+  wires[b3]?.remove(a3);
+
+  // BFS to count the number of reachable nodes from a random single node of the removed pairs
+  var visited = Set<String>();
+  var queue = Queue<String>();
+  queue.add(a1);
+
+  while (queue.isNotEmpty) {
+    var current = queue.removeFirst();
+
+    if (!visited.add(current)) {
+      continue;
+    }
+
+    for (var connection in wires[current] ?? {}) {
+      queue.add(connection);
+    }
+  }
+
+  // Calculate the size of the other subset
+  var part1 = visited.length * (wires.length - visited.length);
+  print("Part 1: $part1");
 }
